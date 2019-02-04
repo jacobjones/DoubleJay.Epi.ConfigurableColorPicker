8d0896c5ea33533fe529bfa10f130034062eccac
jacobjones <jacob.jones@valtech.com>
2019-02-03 22:48:36 -0500

    Fix typo in Assembly Info description

--- DoubleJay.Epi.ConfigurableColorPicker/Properties/AssemblyInfo.cs
+++ DoubleJay.Epi.ConfigurableColorPicker/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DoubleJay.Epi.ConfigurableColorPicker")]
-[assembly: AssemblyDescription("Configruable color picker for Episerver")]
+[assembly: AssemblyDescription("Configurable color picker for Episerver")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jake Jones")]
 [assembly: AssemblyProduct("DoubleJay.Epi.ConfigurableColorPicker")]
