478efdb1e3dc38ee0cc08381f3f302f973dfee58
Jacob Jones <jacob.jones@valtech.com>
2023-01-18 15:21:34 +0100

    Standardize NuGet properties

---
+++ DoubleJay.Epi.ConfigurableColorPicker/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using DoubleJay.Epi.ConfigurableColorPicker.Manager;
+using DoubleJay.Epi.ConfigurableColorPicker.Manager.Caching;
+using EPiServer.Shell.Modules;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DoubleJay.Epi.ConfigurableColorPicker
+{
+    public static class ServiceCollectionExtensions
+    {
+        private const string ModuleName = "DoubleJay.Epi.ConfigurableColorPicker";
+        
+        public static IServiceCollection AddConfigurableColorPicker(this IServiceCollection services)
+        {
+            services.Configure<ProtectedModuleOptions>(
+                pm =>
+                {
+                    if (!pm.Items.Any(i => i.Name.Equals(ModuleName, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        pm.Items.Add(new ModuleDetails {Name = ModuleName});
+                    }
+                });
+
+            services.AddSingleton<IColorPaletteManager, ColorPaletteManagerCachingProxy>();
+
+            return services;
+        }
+    }
+}
